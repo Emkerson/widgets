aef9c889c9874ed0d0be3fa4b5901f165b58ec01
Emkerson <eefendic78@gmail.com>
2022-08-11 22:36:20 +0200

    first commit

---
+++ src/components/Route.js
@@ -0,0 +1,21 @@
+import React,{useEffect, useState} from "react";
+
+
+const Route = ({path,children}) =>{
+    const [currentPath,setCurentPath] = useState(window.location.pathname);
+
+    useEffect (() =>{
+        const onLocationChange = () =>{
+            setCurentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate', onLocationChange);
+
+        return()=>{
+            window.addEventListener('popstate', onLocationChange);
+        }
+    })
+
+    return currentPath === path ?children : null
+};
+
+export default Route;
---
+++ src/components/Search.js
@@ -0,0 +1,125 @@
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+
+const Search =() =>{
+    const [term,setTerm]= useState('programing');
+    const [results, setResults] = useState([]);
+    const [debouncedTerm,setDebouncedTerm] = useState(term);
+    
+    
+    useEffect(()=>{
+        const timerId= setTimeout(()=>{
+            setDebouncedTerm(term);
+        },1000);
+        return()=>{
+            clearTimeout(timerId);
+        };
+    },[term])
+
+    useEffect(()=>{
+        const search = async () =>{
+            const {data} = await axios.get( 'https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm,
+                },
+            });
+            setResults(data.query.search);
+        };
+        
+        search();
+        
+    },[debouncedTerm]);
+
+{/*
+useEffect(()=>{
+        const search = async () =>{
+            const {data} = await axios.get( 'https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
+        if(term && !results.length){
+            search();
+        }else{
+            const timeoutId = setTimeout(()=>{
+                if(term){
+                    search();
+                }
+            },1000); 
+            
+            return() => {
+                clearTimeout(timeoutId);
+            };
+        }  
+    },[term,results.length]);
+*/}
+    
+
+{/*we can do this if we want to use  async and  await
+useEffect(()=>{
+        const search = async () =>{
+            await axios.get( 'Evoooo mene evo menee ovdje sam')
+        }
+
+        search();
+    },[term]);
+
+    we can do this 
+
+    useEffect(()=>{
+       (async()=>{
+        await axios.get('Evo mene evo mene ovdje sam')
+       })();
+    },[term]);*
+    /}
+
+    we can do this 
+    
+    useEffect(()=>{
+        axios.get('Evo mene evoo mene ovdje sam').then((response)=>{
+            console.log(response.data);
+        });
+    },[term]);
+*/}
+
+    const renderedResults = results.map((result)=> {
+        return(
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`} >Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
+
+    return(
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                        className="input"
+                        value={term} 
+                        onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="ui celled list">{renderedResults}</div>
+        </div>
+    )
+}
+export default Search;
